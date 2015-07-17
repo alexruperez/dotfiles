8694300663d561a484323c1272c752180029efa7
alexruperez <alexruperez@gmail.com>
2015-07-17 16:44:18 +0200

    Fix Try I

--- install.sh
+++ install.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 set -e
 
-xcode-select --install
+xcode-select --install || true
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew doctor
-brew update
+brew doctor || true
+brew update || true
 export PATH='/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin'
 
-brew install caskroom/cask/brew-cask
+brew install caskroom/cask/brew-cask || true
 brew tap caskroom/versions
 brew tap caskroom/fonts
-brew cask install iterm2
-brew cask install textmate
-brew cask install sourcetree
-brew install imagemagick
-brew install ffmpeg
+brew cask install iterm2 || true
+brew cask install textmate || true
+brew cask install sourcetree || true
+brew install imagemagick || true
+brew install ffmpeg || true
 
-brew install git
+brew install git || true
 
 git config --global alias.co checkout
 git config --global alias.a add
@@ -35,14 +35,14 @@ git config --global merge.tool "opendiff"
 
 curl -L http://install.ohmyz.sh | sh
 
-brew install rbenv ruby-build
+brew install rbenv ruby-build || true
 
 rbenv install 2.2.2
 rbenv global 2.2.2
 eval "$(rbenv init -)"
 gem install bundler
 
-brew install postgresql
+brew install postgresql || true
 initdb /usr/local/var/postgres
 
 gem install lolcommits
